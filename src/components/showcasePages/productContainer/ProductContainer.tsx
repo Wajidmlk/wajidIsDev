f356afea31326bfe880d86f04dcd5c50418fe214
wjd <wjd@wjd.wjd>
2022-11-30 13:04:10 +0500

    Stripe WIP

--- src/components/showcasePages/productContainer/ProductContainer.tsx
+++ src/components/showcasePages/productContainer/ProductContainer.tsx
@@ -4,7 +4,10 @@ const ProductContainer = () => {
   
   return (
     <div className='product-container'>
-      <div className='side-strip'></div>
+      <div className='side-strip'>
+        <div className='strip-corner-left'></div>
+        <div className='strip-corner-right'></div>
+      </div>
       <div className='badges'></div>
       <div className='images-slider'></div>
       <div className='toggle-button'></div>

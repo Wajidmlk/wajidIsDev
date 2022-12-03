2e7a9c7183d7825cb6041001d14b9050ccc15b5b
wjd <wjd@wjd.wjd>
2022-12-04 03:12:50 +0500

    mui icon importer

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { tpCompId } from '../../common/commonTypes';
+import { tpSliderParams } from '../../common/componentTypes';
 import SliderZero from './SliderZero/SliderZero';
 
-const Slider = (props: tpCompId): JSX.Element => {
+const Slider = (props: tpCompId&tpSliderParams): JSX.Element => {
   let selectedSlider = <></>;
   switch(props.id) {
     case 'slider0':
-      selectedSlider = <SliderZero />;
+      selectedSlider = <SliderZero {...props}/>;
       break;
     default:
-      selectedSlider = <SliderZero />;
+      selectedSlider = <SliderZero {...props}/>;
   }
   return selectedSlider;
 };

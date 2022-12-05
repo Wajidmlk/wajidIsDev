b4663ac81818cc7696d5dd325c75b4ef097a520d
wjd <wjd@wjd.wjd>
2022-12-05 10:26:21 +0500

    bug on prod

--- src/pages/IntroPages/IntroPage.tsx
+++ src/pages/IntroPages/IntroPage.tsx
@@ -4,7 +4,7 @@ import IntroPageZero from './introPageZero/IntroPageZero';
 
 const IntroPage = ({page}: {page: tpPageStructure}) => {
   let pageById: JSX.Element = <></>;
-  switch(page.pageCatId) {
+  switch(page.pageId) {
     case '530d36aa-2aa4-11ed-a261-0242ac121000' :
       pageById = <IntroPageZero page={page} />
       break;

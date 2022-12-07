629ab51d5860b0b30b7def8acde40e5a2d378b81
wjd <wjd@wjd.wjd>
2022-12-07 16:15:53 +0500

    fix

--- src/pages/showcasePages/productContainer/ProductContainer.tsx
+++ src/pages/showcasePages/productContainer/ProductContainer.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { MUI_ICON } from '../../../appUtils/AppUtilities';
 import { tpProduct, tpProductsState } from '../../../common/componentTypes';
 import { Button } from '../../../components/buttons/Button';

c6ab8c881396fc037992c505bb4f14858aadbb20
wjd <wjd@wjd.wjd>
2022-12-23 12:10:33 +0500

    Gallery two

--- src/components/galleries/Gallery.tsx
+++ src/components/galleries/Gallery.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { tpCompId } from '../../common/commonTypes';
 import { tpGallery } from '../../common/componentTypes';
 import GalleryOne from './galleryOne/GalleryOne';
+import GalleryTwo from './galleryTwo/GalleryTwo';
 
 const Gallery = (props: tpCompId&tpGallery): JSX.Element => {
   const isMobileMode = window.innerWidth < 550;
   const {id, items, className} = props;
   let selectedComp = <></>;
   switch(id) {
-    case 'gallery-000':
+    case 'gallery-001':
       selectedComp = <GalleryOne {...props} />;
+      break;
+    case 'gallery-002':
+      selectedComp = <GalleryTwo {...props} />;
       break;
     default:
       selectedComp = <GalleryOne {...props} />;

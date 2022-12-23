de4c6070f2539abc6019ab5ac89f543156e11c5c
wjd <wjd@wjd.wjd>
2022-12-23 22:43:11 +0500

    patches

--- src/common/staticApp/AppStaticData.ts
+++ src/common/staticApp/AppStaticData.ts
@@ -1,13 +1,2 @@
 
-export const userIntroData = {
-  title: `It's Malik Wajid`,
-  animated: {
-      first: 'I am MERN Stack Developer....',
-      second: '3+ Years of experience...',
-      third: '30+ successful projects....',
-  },
-  description: `Checkout my portfolio for products, Or Hit Contact Me for special requirements, custom apps or discussion`,
-  imageUrl: 'https://images.unsplash.com/photo-1514790193030-c89d266d5a9d',
-};
-
 export const DEFAULT_IMAGE_URL = 'https://www.pngitem.com/pimgs/m/35-350426_profile-icon-png-default-profile-picture-png-transparent.png';

6b696a40668f62541c076f8e9b9a7c1d078f4f22
wjd <wjd@wjd.wjd>
2022-12-29 00:10:03 +0500

    s

--- src/components/galleries/serviceGallery.tsx
+++ src/components/galleries/serviceGallery.tsx
@@ -24,7 +24,10 @@ export const ServiceGallery2Maker = ({items, noOfRows}: tpGallery) => {
   const table: JSX.Element[] = [];
   const tableRows: JSX.Element[] = items.map(({id, src, value}) => ( 
     <div key={id} className ="box">
-      <img src={src} />
+      <img
+        src={src}
+        alt={value}
+      />
       <span>{value}</span>
     </div>
   ));
@@ -40,7 +43,11 @@ export const ServiceGallery2Maker = ({items, noOfRows}: tpGallery) => {
 export const ServiceGallery3Maker = ({items, noOfRows}: tpGallery) => {
   const table: JSX.Element[] = [];
   const tableRows: JSX.Element[] = items.map(({id, src, value}) => ( 
-    <img src={src} />
+    <img
+      key={id}
+      src={src}
+      alt={value}
+    />
   ));
   const dataInRow = Math.round(tableRows.length / noOfRows);
     for(let x = 0; x < noOfRows; x++) {
